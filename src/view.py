7b67089501f43b7ba5231ba1493200741f083b16
Noam Levy <noam5003@gmail.com>
2023-03-08 16:26:50 +0200

    view configuration as subject and boilerplate

--- src/view.py
+++ src/view.py
@@ -1,20 +1,43 @@
 import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from tkinter import PhotoImage
 
-class View:
+from abstractSubject import AbstractSubject
+from abstractObserver import AbstractObserver
+
+class View(AbstractSubject):
 
   def __init__(self):
+    self._observers: list[AbstractObserver] = []
     self.root = ttk.Window(themename="superhero")
     self.root.title("The 100 Prisoners")
-    self.root.iconbitmap('./src/images/prison.png')
+    photo = PhotoImage(file = './src/images/prison.png')
+    self.root.iconphoto(False, photo)
     self.root.geometry("1200x600")
     self._drawMenuDrawer()
+    self._drawBoxMatrix()
 
   def _drawMenuDrawer(self):
     pass
-
+  
+  def _drawBoxMatrix(self):
+    matrix_frame = ttk.Frame(self.root, bootstyle=LIGHT)
+    label = ttk.Label(matrix_frame, text="Simulation viewer")
+    label.pack()
+    matrix_frame.pack()
 
   def run(self):
     self.root.mainloop()
+
+  def attach(self, observer: AbstractObserver):
+    self._observers.append(observer)
+
+  def detach(self, observer: AbstractObserver):
+    self._observers.remove(observer)
+  
+  def notify(self):
+    for observer in self._observers:
+      observer.update(self) # TODO: consider notify implementation.
 
 if __name__ == "__main__":
   root = View()
