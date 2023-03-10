505d9bf28c04b52916f7f55f8172c2a40e126e47
Noam Levy <noam5003@gmail.com>
2023-03-10 15:31:12 +0200

    refactored menu drawer. resized prisoner drawing

--- src/view.py
+++ src/view.py
@@ -6,60 +6,43 @@ from listener import UIEventsListener
 
 DEFAULT_FONT = "Helvetica"
 DEFAULT_FONT_SIZE = 10
+DEFAULT_PADDING = 10
 
 class View():
 
   def __init__(self):
     self._observers: list[UIEventsListener] = []
-    self.root = ttk.Window(themename="superhero")
-    self.root.title("The 100 Prisoners")
-    photo = PhotoImage(file = './src/images/prison.png')
-    self.root.iconphoto(False, photo)
-    self.root.geometry("1200x600")
+    self.root = ttk.Window(title="The 100 Prisoners", themename="superhero", size=(1200,600), iconphoto='./src/images/prison.png')
     self.run()
 
   def _drawMenuDrawer(self):
-    settings_frame = ttk.Frame(self.root, bootstyle=LIGHT)
-    statistics_label = ttk.Label(settings_frame,text = "Statistics", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE + 5), bootstyle=(LIGHT, INVERSE))
+    menu_frame = ttk.Frame(self.root, bootstyle=LIGHT)
+    statistics_frame = self._drawStatisticsFrame(menu_frame)
+    prisoner_data_frame = self._drawPrisonersDataFrame(menu_frame)
+    # simulation_settings_frame = self._drawSimulationSettingsFrame(menu_frame)
+    statistics_frame.pack(ipadx=DEFAULT_PADDING)
+    ttk.Separator(menu_frame, bootstyle=SECONDARY).pack(pady=DEFAULT_PADDING, fill=X)
+    prisoner_data_frame.pack(ipadx=DEFAULT_PADDING)
+    ttk.Separator(menu_frame, bootstyle=SECONDARY).pack(pady=DEFAULT_PADDING, fill=X)
+    menu_frame.pack(side=LEFT, fill=Y)
 
-    prisonerNum_label = ttk.Label(settings_frame,text = "Prisoner Number :", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),bootstyle =(LIGHT, INVERSE))
-    boxNum_label = ttk.Label(settings_frame,text = "Box Number :", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),bootstyle=(LIGHT, INVERSE))
-    foundNum_label = ttk.Label(settings_frame,text = "Found Number :", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),bootstyle=(LIGHT, INVERSE))
-    guessNum_label = ttk.Label(settings_frame,text = "Number of Guesses :", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE),bootstyle=(LIGHT, INVERSE))
-
-    setting_label = ttk.Label(settings_frame,text = "Settings", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE + 5), bootstyle=(LIGHT, INVERSE))
-
-    priNum_label = ttk.Label(settings_frame,text = "# of prisoners", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle=(LIGHT, INVERSE))
-    priNum_scroll = ttk.Scrollbar(settings_frame, orient=HORIZONTAL, bootstyle="secondary")
-
-    simNum_label = ttk.Label(settings_frame,text = "# of simulations", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle=(LIGHT, INVERSE))
-
-    random_label = ttk.Checkbutton(settings_frame,text = "select randomly", bootstyle="SECONDARY-ROUND-TOGGLE")
-    strategy_label = ttk.Checkbutton(settings_frame,text = "apply strategy", bootstyle="SECONDARY-ROUND-TOGGLE")
-    start_button = ttk.Button(settings_frame,text = "Start",bootstyle="success", command=self.on_start)
-    quit_button = ttk.Button(settings_frame,text = "Quit", bootstyle="danger", command=self.on_quit)
-
-    settings_frame.pack(side=LEFT, fill=Y)
-    statistics_label.pack(padx=10, pady=10)
-    ttk.Separator(settings_frame, bootstyle=SECONDARY).pack(side=LEFT, padx=10,pady=10)
-    prisonerNum_label.pack(padx=10, pady=(400,10))
-
-    boxNum_label.pack(padx=10, pady=10)
-    foundNum_label.pack(padx=10, pady=10)
-    guessNum_label.pack(padx=10, pady=10)
+  def _drawPrisonersDataFrame(self, parent_frame):
+    data_frame = ttk.Frame(parent_frame, bootstyle=LIGHT)
+    img = PhotoImage(file='./src/images/prisoner.png')
+    img_label = ttk.Label(data_frame, image=img, bootstyle=(LIGHT, INVERSE))
+    img_label.pack(side=LEFT, padx=DEFAULT_PADDING)
+    img_label.image = img
+    ttk.Label(data_frame, text = "Prisoner Number:", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle =(LIGHT, INVERSE)).pack(anchor=W)
+    ttk.Label(data_frame, text = "Box Number:", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle=(LIGHT, INVERSE)).pack(anchor=W)
+    ttk.Label(data_frame, text = "Found Number:", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle=(LIGHT, INVERSE)).pack(anchor=W)
+    ttk.Label(data_frame, text = "Number of Guesses:", font=(DEFAULT_FONT, DEFAULT_FONT_SIZE), bootstyle=(LIGHT, INVERSE)).pack(anchor=W)
     
+    return data_frame
 
-    ttk.Separator(settings_frame, bootstyle=SECONDARY).pack(side=LEFT, padx=10,pady=10)
-    setting_label.pack(padx=10, pady=10)
-    priNum_label.pack(padx=10, pady=10)
-    priNum_scroll.pack(side=LEFT)
-    simNum_label.pack(padx=10, pady=10)
-
-    random_label.pack(padx=10, pady=10)
-    strategy_label.pack(padx=10, pady=10)
-    start_button.pack(side=LEFT, expand=YES, padx=10, pady=(15,10))
-    quit_button.pack(side=LEFT, expand=YES, padx=10, pady=(15,10))
-
+  def _drawStatisticsFrame(self, parent_frame):
+    statistics_frame = ttk.Frame(parent_frame, bootstyle=LIGHT)
+    ttk.Label(statistics_frame, text = "Simulation Statistics", font=(DEFAULT_FONT, 2*DEFAULT_FONT_SIZE), bootstyle =(LIGHT, INVERSE)).pack()
+    return statistics_frame
 
   def _drawBoxMatrix(self):
     matrix_frame = ttk.Frame(self.root, bootstyle=LIGHT)
@@ -78,8 +61,6 @@ class View():
   def detach(self, observer: UIEventsListener):
     self._observers.remove(observer)
 
-  
-  
   def on_start(self):
     return
 
